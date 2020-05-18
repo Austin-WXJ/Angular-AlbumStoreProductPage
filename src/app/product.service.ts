27ba5ab579047a4299d9c3e711f2ff017e7b20d8
Austin <austin.wxj@gmail.com>
2020-05-18 10:54:26 -0400

    submitting for module 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
-import { Album } from './models/album.model';
-
+import { Album } from './models/album';
 @Injectable()
 export class ProductService {
 
